a0b1f3841bfff78c66065561b0e12240b681ff12
kvs94 <vitaliykucher94@gmail.com>
2021-01-30 03:42:00 +0200

    finish base framework functional

---
+++ src/components/header/Header.js
@@ -0,0 +1,23 @@
+import {ExcelCompoenet} from '@core/ExcelComponent'
+
+export class Header extends ExcelCompoenet {
+  static className = 'excel__header'
+
+  toHTML() {
+    return `
+      <input type="text" class="input" value="Новая таблица">
+
+      <div>
+
+        <div class="button">
+          <i class="material-icons">exit_to_app</i>
+        </div>
+
+        <div class="button">
+          <i class="material-icons">delete</i>
+        </div>
+
+      </div>
+    `
+  }
+}
---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,20 @@
+import {DomListener} from './DomListener'
+
+export class ExcelCompoenet extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+  }
+
+  toHTML() {
+    return ''
+  }
+
+  init() {
+    this.initDOMListeners()
+  }
+
+  destroy() {
+    this.removeDOMListeners()
+  }
+}
